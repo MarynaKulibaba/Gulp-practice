6cf1d9492eeabe49fa3fb828a72a07f16454d871
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-07-19 19:20:42 +0300

    practice with Globs, copying js files to directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,72 @@
-const {
-    src,
-    dest,
-    parallel,
-    series,
-    watch
-} = require('gulp');
-
-// Load plugins
-
+const { src, dest, parallel, series, watch } = require('gulp');
 const cssnano = require('gulp-cssnano');
 const changed = require('gulp-changed');
 const browsersync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-
-
+// Очищення директорії build
 function clear() {
-    return src('./build/*', {
-        read: false
-    })
-        .pipe(clean());
+    return src('build/*', { read: false }).pipe(clean());
 }
 
-// CSS 
-
+// CSS
 function css() {
-    const source = './src/css/style.css';
-
-    return src(source)
-        .pipe(changed(source))
+    return src('src/css/*.css')
+        .pipe(changed('build/css'))
         .pipe(cssnano())
-        .pipe(dest('./build/css/'))
+        .pipe(dest('build/css'))
         .pipe(browsersync.stream());
 }
 
-// Optimize images
+// JavaScript
+function scripts() {
+    return src('src/scripts/*.js')
+        .pipe(changed('build/js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('build/js'));
+}
 
-function img() {
-    return src('./src/images/*')
+// Оптимізація images
+function images() {
+    return src('src/images/*')
         .pipe(imagemin())
-        .pipe(dest('./build/images'));
+        .pipe(dest('build/images'));
 }
 
-// html
-
+// copy HTML
 function html() {
-    return src('./src/*.html')
-        .pipe(dest('./build/'))
+    return src('src/*.html')
+        .pipe(dest('build'))
         .pipe(browsersync.stream());
 }
 
-// Watch files
-
-function watchFiles() {
-    watch('./src/css/*', css);
-    watch('./src/*.html', html);
-    watch('./src/images/*', img);
+// copy fonts
+function fonts() {
+    return src('src/fonts/*')
+        .pipe(dest('build/fonts'));
 }
 
-// BrowserSync
+// copy JS файлів з src/scripts до build/js і src/scripts/deleted
+function copyScripts() {
+    return src('src/scripts/*.js')
+        .pipe(dest('build/js'))
+        .pipe(dest('src/scripts/deleted'));
+}
 
+// Track changes in files
+function watchFiles() {
+    watch('src/css/*.css', css);
+    watch('src/scripts/*.js', scripts);
+    watch('src/images/*', images);
+    watch('src/fonts/*', fonts);
+    watch('src/*.html', html);
+}
+
+// setting BrowserSync
 function browserSync() {
     browsersync.init({
         server: {
@@ -70,5 +76,12 @@ function browserSync() {
     });
 }
 
+exports.clear = clear;
+exports.css = css;
+exports.images = images;
+exports.scripts = scripts;
+exports.html = html;
+exports.fonts = fonts;
+exports.copyScripts = copyScripts;
 exports.watch = parallel(watchFiles, browserSync);
-exports.default = series(clear, parallel(html, css, img));
+exports.default = series(clear, parallel(html, css, images, fonts, scripts, copyScripts));
