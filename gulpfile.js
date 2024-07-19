6b33a973dacc1c4cd684eeb417a1f0ea1e580e86
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-07-20 00:50:44 +0300

    development task for SASS, Pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,49 @@
-const {
-    src,
-    dest,
-    parallel,
-    series,
-    watch
-} = require('gulp');
-
-// Load plugins
-
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
 const cssnano = require('gulp-cssnano');
 const changed = require('gulp-changed');
 const browsersync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
 
-
-
+// clean directory build
 function clear() {
-    return src('./build/*', {
-        read: false
-    })
-        .pipe(clean());
+    return src('./build/*', { read: false }).pipe(clean());
 }
 
-// CSS 
-
-function css() {
-    const source = './src/css/style.css';
-
-    return src(source)
-        .pipe(changed(source))
+// Компіляція SASS
+function styles() {
+    return src('./src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(cssnano())
-        .pipe(dest('./build/css/'))
+        .pipe(dest('./build/css'))
+        .pipe(browsersync.stream());
+}
+
+// Компіляція Pug
+function templates() {
+    return src('./src/pug/*.pug')
+        .pipe(pug())
+        .pipe(dest('./build'))
         .pipe(browsersync.stream());
 }
 
 // Optimize images
-
 function img() {
     return src('./src/images/*')
         .pipe(imagemin())
         .pipe(dest('./build/images'));
 }
 
-// html
-
-function html() {
-    return src('./src/*.html')
-        .pipe(dest('./build/'))
-        .pipe(browsersync.stream());
-}
-
-// Watch files
-
+// Watch files and directories
 function watchFiles() {
-    watch('./src/css/*', css);
-    watch('./src/*.html', html);
+    watch('./src/scss/*.scss', styles);
+    watch('./src/pug/*.pug', templates);
     watch('./src/images/*', img);
 }
 
 // BrowserSync
-
 function browserSync() {
     browsersync.init({
         server: {
@@ -70,5 +53,18 @@ function browserSync() {
     });
 }
 
+// Development task
+const development = series(
+    clear,
+    parallel(styles, templates, img),
+    parallel(watchFiles, browserSync)
+);
+
+// Other exports
+exports.clear = clear;
+exports.styles = styles;
+exports.templates = templates;
+exports.images = img;
 exports.watch = parallel(watchFiles, browserSync);
-exports.default = series(clear, parallel(html, css, img));
+exports.default = series(clear, parallel(templates, styles, img));
+exports.development = development;
